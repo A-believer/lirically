451f8daf0b6aa2d58d7f879a8807e19898d35396
davedev <davidabolade29@gmail.com>
2023-02-14 10:45:34 +0100

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="text-white bg-black h-screen text-center">
+      Hello World!
+    </div>
+  )
+}
+
+export default App
