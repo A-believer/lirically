02d0f1d8994fd42b0caa7fa66e7176740149bb33
davedev <davidabolade29@gmail.com>
2023-02-14 11:47:01 +0100

    file setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Routes, Route } from "react-router-dom"
+import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components'
+
+import {ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts} from './pages'
 
 function App() {
 
   return (
-    <div className="text-white bg-black h-screen text-center">
-      Hello World!
-    </div>
+    <main>
+      <Sidebar />
+      <div>
+        <Searchbar />
+        
+        <div>
+          <div>
+            <Routes>
+              <Route path="/" element={<Discover/>}/>
+              <Route path="/top-artists" element={<TopArtists/>}/>
+              <Route path="/top-charts" element={<TopCharts/>}/>
+              <Route path="/around-you" element={<AroundYou/>}/>
+              <Route path="/artists/:id" element={<ArtistDetails/>}/>
+              <Route path="/song/:songid" element={<SongDetails/>}/>
+              <Route path="/search/:searchTerm" element={<Search/>}/>
+            </Routes>
+          </div>
+          <div>
+            <TopPlay/>
+          </div>
+        </div>
+      </div>
+      
+
+      {activeSong?.title && (
+        <div>
+          <MusicPlayer/>
+        </div>
+      )}
+    </main>
   )
 }
 
