e8cc066e2c7112f0cb0a05627b388b98c7094ac7
davedev <davidabolade29@gmail.com>
2023-02-25 11:51:05 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components'
 import {ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts} from './pages'
 
 function App() {
+  const { activeSong } = useSelector((state) => state.player);
 
   return (
-    <main>
+    <main className='relative flex'>
       <Sidebar />
-      <div>
+      <div className='flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]'>
         <Searchbar />
         
-        <div>
-          <div>
+        <div className='px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse'>
+          <div className='flex-1 h-fit pb-40'>
             <Routes>
               <Route path="/" element={<Discover/>}/>
               <Route path="/top-artists" element={<TopArtists/>}/>
@@ -24,7 +25,7 @@ function App() {
               <Route path="/search/:searchTerm" element={<Search/>}/>
             </Routes>
           </div>
-          <div>
+          <div className='xl:sticky relative top-0 h-fit'>
             <TopPlay/>
           </div>
         </div>
@@ -32,7 +33,7 @@ function App() {
       
 
       {activeSong?.title && (
-        <div>
+        <div className='absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg z-10 rounded-t-3xl'>
           <MusicPlayer/>
         </div>
       )}
